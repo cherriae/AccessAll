b7a867e63583dfbafca9bc46ac67ea2c24360186
Jerry <zeruiz@nycstudents.net>
2026-07-30 14:06:07 -0400

    add(Template)

---
+++ README.md
@@ -0,0 +1,1 @@
+# AccessAll
---
+++ src/app/index.tsx
@@ -0,0 +1,15 @@
+import * as Device from 'expo-device';
+import { Platform, StyleSheet } from 'react-native';
+
+import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
+
+
+export default function HomeScreen() {
+  return (
+    <div>
+        Hello World
+    </div>
+  );
+}
+
+const styles = StyleSheet.create({});
